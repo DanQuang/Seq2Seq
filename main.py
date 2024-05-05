3456ff46afb148f45d001a8825fb453d7fd345c2
DanQuang <21521917@gm.uit.edu.vn>
2024-05-05 21:05:42 +0700

    add main

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import yaml # type: ignore
+import argparse
+import logging
+from typing import Text
+
+from train import Train_Task
+
+def main(config_path: Text) -> None:
+    logging.basicConfig(level=logging.INFO)
+
+    with open(config_path) as f:
+        config = yaml.safe_load(f)
+
+    logging.info("Training started...")
+    Train_Task(config).train()
+
+    logging.info("Train complete")
+
+    # logging.info("Evaluating...")
+    # Test_Task(config).predict()
+    # logging.info("Task done!!!")
+
+if __name__ == "__main__":
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--config', dest='config', required=True)
+    args = args_parser.parse_args()
+    
+    main(args.config)
--- test.py
+++
@@ -1,5 +0,0 @@
-import datasets
-
-dataset = datasets.load_dataset("bentrevett/multi30k")
-
-print(type(dataset["train"]))
